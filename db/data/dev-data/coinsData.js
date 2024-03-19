b644ded98984112a84c43291cca1086473af8f65
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-03-19 14:12:11 +0000

    add clients changes to data structure to remove prices into seperate table

--- db/data/dev-data/coinsData.js
+++ db/data/dev-data/coinsData.js
@@ -1,1 +1,10 @@
-module.exports = [];
+module.exports = [
+  {
+    "coin_id": 1,
+    "coin_name": "BTC"
+  },
+  {
+    "coin_id": 26081,
+    "coin_name": "FDUSD"
+  }
+];
--- db/data/dev-data/pairsData.js
+++ db/data/dev-data/pairsData.js
@@ -4,17 +4,5 @@ module.exports = [
     "pair_name": "BTC/FDUSD",
     "base_asset": "BTC",
     "quote_asset": "FDUSD"
-  },
-  {
-    "pair_id": 9933,
-    "pair_name": "BTC/USDT",
-    "base_asset": "BTC",
-    "quote_asset": "USDT"
-  },
-  {
-    "pair_id": 1261061,
-    "pair_name": "FDUSD/USDT",
-    "base_asset": "FDUSD",
-    "quote_asset": "USDT"
   }
 ];
