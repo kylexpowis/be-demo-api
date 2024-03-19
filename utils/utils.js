b5c19aecfcc5e843aa61462a1bc5bee7971d2ebd
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-03-19 14:11:06 +0000

    adding clients changes to data structure

--- utils/utils.js
+++ utils/utils.js
@@ -51,11 +51,6 @@ exports.extractPairs = (data) => {
                 pair_name: item.market_pair,
                 base_asset: item.market_pair_base.currency_symbol,
                 quote_asset: item.market_pair_quote.currency_symbol,
-                price: item.quote.exchange_reported.price,
-                volume_24h: item.quote.exchange_reported.volume_24h,
-                depth_negative_two: item.quote.exchange_reported.depth_negative_two,
-                depth_positive_two: item.quote.exchange_reported.depth_positive_two,
-                last_updated: item.quote.exchange_reported.last_updated
             });
             seen.add(item.market_id);
         }
