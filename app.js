0be97f6ecff51ac24e325a375ffcd66cc68122ee
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-03-21 21:21:55 +0000

    remove unused endpoints

--- app.js
+++ app.js
@@ -5,18 +5,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/api/checkForListings");
-app.get("/api");
-app.get("/api/coins");
-app.get("/api/coins/:coin_name");
-app.get("/api/coins/:coin_id");
-app.get("/api/coins_new");
-app.get("/api/coins/:coin_name/pairs");
-app.get("/api/pairs");
-app.get("/api/pairs/pair_id");
-app.get("/api/metrics");
-app.get("/api/metrics/coin_id");
-
 app.all("/*", (req, res, next) => {
   res.status(404).send({ msg: "Path not found" });
 });
