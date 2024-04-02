04790f5b7920fc1ffb7d76bb6158fa0421d225f0
imaa04 <imakhan.04@gmail.com>
2024-04-02 18:31:51 +0100

    create endpoint for 24 hour volume roc percentage with 200 status test

--- models/closingMCap.model.js
+++ models/closingMCap.model.js
@@ -20,3 +20,33 @@ WHERE
     return rows;
   });
 };
+
+exports.fetchVolumeROC = () => {
+  const queryString = `
+  SELECT
+    c.coin_id,
+    c.symbol,
+    c.coin_name,
+    c.logo_url,
+    t.vol_percentage_change,
+    v.volume_over_marketcap
+FROM
+    coins c
+JOIN
+    tradeinfo t ON t.coin_id = c.coin_id
+JOIN (
+    SELECT
+        coin_id,
+        volume_over_marketcap,
+        timestamp
+    FROM
+        vol24marketcap v1
+    WHERE
+        v1.timestamp = (SELECT MAX(v2.timestamp)
+                        FROM vol24marketcap v2
+                        WHERE v1.coin_id = v2.coin_id)
+) v ON v.coin_id = c.coin_id AND c.currency_type = 'cryptocurrency'`;
+  return db.query(queryString).then(({ rows }) => {
+    return rows;
+})
+}
