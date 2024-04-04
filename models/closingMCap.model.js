c3fe1b2bdd817824d72bb4f502f8ca3724dc589c
imaa04 <imakhan.04@gmail.com>
2024-04-04 20:50:27 +0100

    change psql code for rocmarketcap model

--- models/closingMCap.model.js
+++ models/closingMCap.model.js
@@ -1,22 +1,32 @@
 const db = require("../db/connection");
 
 exports.fetchROCMarketCap = () => {
-  const queryString = `SELECT DISTINCT ON (cm.coin_id)
-  cm.coin_id,
-  cm.timestamp,
-  (
-    (ti.current_marketcap - cm.closing_marketcap) / cm.closing_marketcap * 100
-  ) AS percentage_change
-  FROM
-  closing_marketcap cm
-  JOIN
-  tradeinfo ti ON cm.coin_id = ti.coin_id
-  JOIN
-  coins c ON cm.coin_id = c.coin_id
-  WHERE
-  c.currency_type = 'cryptocurrency'
-  ORDER BY
-  cm.coin_id, cm.timestamp DESC;`;
+  const queryString = `
+  SELECT
+    c.coin_id,
+    c.symbol,
+    c.coin_name,
+    c.logo_url,
+    ROUND(((ti.current_marketcap - cm.closing_marketcap) / cm.closing_marketcap) * 100, 2) AS marketcap_percentage_change,
+    ti.timestamp AS latest_timestamp
+FROM
+    coins c
+INNER JOIN
+    closing_marketcap cm ON c.coin_id = cm.coin_id
+INNER JOIN
+    tradeinfo ti ON c.coin_id = ti.coin_id
+INNER JOIN
+    (SELECT
+        coin_id,
+        MAX(timestamp) AS MaxTimestamp
+     FROM
+        tradeinfo
+     GROUP BY
+        coin_id) tm ON ti.coin_id = tm.coin_id AND ti.timestamp = tm.MaxTimestamp
+WHERE
+    c.is_active = TRUE
+ORDER BY
+    c.coin_id;`;
   return db.query(queryString).then(({ rows }) => rows);
 };
 
